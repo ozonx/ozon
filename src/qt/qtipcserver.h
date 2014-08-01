4c8d0b89446c2831511218b1e9d82e350a70a0fd
ozon <jsakmnm@hotmail.com>
2014-08-02 02:31:40 +0400

    OZON

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Ozoncoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "OzoncoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
